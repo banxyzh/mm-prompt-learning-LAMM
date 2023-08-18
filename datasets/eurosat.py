f9ef9b5e4536e6549ca615318dc7b18f82d995ba
banxyzh <banxyzh@proton.me>
2023-08-18 14:23:38 +0000

    Version 1.10.1

--- datasets/eurosat.py
+++ datasets/eurosat.py
@@ -7,32 +7,37 @@ from dassl.utils import mkdir_if_missing
 from .oxford_pets import OxfordPets
 from .dtd import DescribableTextures as DTD
 
-IGNORED = ["BACKGROUND_Google", "Faces_easy"]
 NEW_CNAMES = {
-    "airplanes": "airplane",
-    "Faces": "face",
-    "Leopards": "leopard",
-    "Motorbikes": "motorbike",
+    "AnnualCrop": "Annual Crop Land",
+    "Forest": "Forest",
+    "HerbaceousVegetation": "Herbaceous Vegetation Land",
+    "Highway": "Highway or Road",
+    "Industrial": "Industrial Buildings",
+    "Pasture": "Pasture Land",
+    "PermanentCrop": "Permanent Crop Land",
+    "Residential": "Residential Buildings",
+    "River": "River",
+    "SeaLake": "Sea or Lake",
 }
 
 
 @DATASET_REGISTRY.register()
-class Caltech101(DatasetBase):
+class EuroSAT(DatasetBase):
 
-    dataset_dir = "caltech-101"
+    dataset_dir = "eurosat"
 
     def __init__(self, cfg):
         root = os.path.abspath(os.path.expanduser(cfg.DATASET.ROOT))
         self.dataset_dir = os.path.join(root, self.dataset_dir)
-        self.image_dir = os.path.join(self.dataset_dir, "101_ObjectCategories")
-        self.split_path = os.path.join(self.dataset_dir, "split_zhou_Caltech101.json")
+        self.image_dir = os.path.join(self.dataset_dir, "2750")
+        self.split_path = os.path.join(self.dataset_dir, "split_zhou_EuroSAT.json")
         self.split_fewshot_dir = os.path.join(self.dataset_dir, "split_fewshot")
         mkdir_if_missing(self.split_fewshot_dir)
 
         if os.path.exists(self.split_path):
             train, val, test = OxfordPets.read_split(self.split_path, self.image_dir)
         else:
-            train, val, test = DTD.read_and_split_data(self.image_dir, ignored=IGNORED, new_cnames=NEW_CNAMES)
+            train, val, test = DTD.read_and_split_data(self.image_dir, new_cnames=NEW_CNAMES)
             OxfordPets.save_split(train, val, test, self.split_path, self.image_dir)
 
         num_shots = cfg.DATASET.NUM_SHOTS
@@ -57,3 +62,12 @@ class Caltech101(DatasetBase):
         train, val, test = OxfordPets.subsample_classes(train, val, test, subsample=subsample)
 
         super().__init__(train_x=train, val=val, test=test)
+
+    def update_classname(self, dataset_old):
+        dataset_new = []
+        for item_old in dataset_old:
+            cname_old = item_old.classname
+            cname_new = NEW_CLASSNAMES[cname_old]
+            item_new = Datum(impath=item_old.impath, label=item_old.label, classname=cname_new)
+            dataset_new.append(item_new)
+        return dataset_new
---
+++ datasets/food101.py
@@ -0,0 +1,25 @@
+import os
+import pickle
+
+from dassl.data.datasets import DATASET_REGISTRY, Datum, DatasetBase
+from dassl.utils import mkdir_if_missing
+
+from .oxford_pets import OxfordPets
+from .dtd import DescribableTextures as DTD
+
+
+@DATASET_REGISTRY.register()
+class Food101(DatasetBase):
+
+    dataset_dir = "food-101"
+
+    def __init__(self, cfg):
+        root = os.path.abspath(os.path.expanduser(cfg.DATASET.ROOT))
+        self.dataset_dir = os.path.join(root, self.dataset_dir)
+        self.image_dir = os.path.join(self.dataset_dir, "images")
+        self.split_path = os.path.join(self.dataset_dir, "split_zhou_Food101.json")
+        self.split_fewshot_dir = os.path.join(self.dataset_dir, "split_fewshot")
+        mkdir_if_missing(self.split_fewshot_dir)
+
+        if os.path.exists(self.split_path):
+            train, val, test = OxfordP
